83122e29b8f915d80fc49b271c988c34d0732c80
JeromeNL <jw.kwetters@student.avans.nl>
2024-03-10 10:48:00 +0100

    dark mode switch

--- app/src/main/java/nl/joramkwetters/duogerman/MainActivity.kt
+++ app/src/main/java/nl/joramkwetters/duogerman/MainActivity.kt
@@ -3,11 +3,19 @@ package nl.joramkwetters.duogerman
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Switch
+import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import nl.joramkwetters.duogerman.Navigation.AppNavigation
@@ -17,13 +25,25 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            DuoGermanTheme {
+            var darkTheme by remember { mutableStateOf(false) }
+
+            DuoGermanTheme(darkTheme = darkTheme) {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    AppNavigation()
+                    Column(){
+                        Row(){
+                            ThemeSwitcher(darkTheme = darkTheme ) {
+                                darkTheme = !darkTheme
+                            }
+                        }
+                        Row(){
+                            AppNavigation()
+                        }
+                    }
+
                 }
             }
         }
@@ -44,4 +64,18 @@ fun GreetingPreview() {
     DuoGermanTheme {
         AppNavigation()
     }
+}
+
+@Composable
+fun ThemeSwitcher(darkTheme: Boolean, onThemeChange : () -> Unit) {
+    Switch(
+        checked = darkTheme,
+        onCheckedChange = { onThemeChange() },
+        colors = SwitchDefaults.colors(
+            checkedTrackColor = MaterialTheme.colorScheme.secondaryContainer,
+            checkedThumbColor = MaterialTheme.colorScheme.primary,
+            uncheckedTrackColor = MaterialTheme.colorScheme.secondaryContainer,
+            uncheckedThumbColor = MaterialTheme.colorScheme.primary
+        )
+    )
 }
